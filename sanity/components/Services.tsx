00862ef33c97406aee0d3bac2e3201664bd94ff9
zack <zackzbradshaw@gmail.com>
2023-01-31 20:12:32 -0600

    Created Services section

--- sanity/components/Services.tsx
+++ sanity/components/Services.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Card from './Card'
+import React from 'react';
+import Card from './Card';
 
-const Services = () => {
+function Services() {
   return (
-    <div className="flex justify-around p-5">
-     <Card image="https://picsum.photos/seed/picsum/800/800" title="Basic" />
-      <Card image="https://picsum.photos/seed/picsum/800/800" title="Normal" />
-      <Card image="https://picsum.photos/seed/picsum/800/800" title="Advanced" />
+    <div className="flex justify-center">
+      <div className="flex justify-around max-w-screen-2xl m-5 w-full ">
+        <Card image="https://picsum.photos/seed/picsum/800/800" title="Basic" />
+        <Card image="https://picsum.photos/seed/picsum/800/800" title="Normal" />
+        <Card image="https://picsum.photos/seed/picsum/800/800" title="Advanced" />
+      </div>
     </div>
-   );
-}; 
+  );
+}
 export default Services;
