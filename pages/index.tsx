889d02a248a1fc5a98f0692695abea2cc2c10783
zack <zackzbradshaw@gmail.com>
2023-01-31 18:09:14 -0600

    Created basic hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Navbar from '../sanity/components/Navbar'
 import Layout from '../sanity/components/Layout'
+import Hero from '../sanity/components/Hero'
 
 export default function IndexPage() {
   return (
     <>
       <Layout>
+        <Hero />
       </Layout>
     </>
   )
