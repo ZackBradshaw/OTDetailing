6a036f22b502092d825f8689667ee54f80a17bbb
zack <zackzbradshaw@gmail.com>
2023-02-01 18:53:10 -0600

    Created Basic footer

--- sanity/components/Layout.tsx
+++ sanity/components/Layout.tsx
@@ -1,10 +1,12 @@
 import Navbar from './Navbar';
+import Footer from './Footer'
 
 function Layout({ children }) {
   return (
     <div className="layout">
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 }
