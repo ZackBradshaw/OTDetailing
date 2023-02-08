b7d20c22d48cc7f873e8b1f4988c270e2cd218d7
zack <zackzbradshaw@gmail.com>
2023-02-08 17:54:47 -0600

    Added more cards and discriptions

--- sanity/components/Services.tsx
+++ sanity/components/Services.tsx
@@ -5,9 +5,11 @@ function Services() {
   return (
     <div className='flex justify-center items-center bg-black align-center'>
       <div className=' flex justify-around align-center gap-3 items-center max-w-screen-2xl max-[800px]:flex-col m-5 w-full '>
-        <Card image='https://picsum.photos/seed/picsum/800/800' title='Basic' />
-        <Card image='https://picsum.photos/seed/picsum/800/800' title='Normal' />
-        <Card image='https://picsum.photos/seed/picsum/800/800' title='Advanced' />
+
+        <Card title='Basic Exterior' description=' T3 Conditioner | Wheel Cleaner'/>
+        <Card title='Exterior#2' description='HotShine® Carnauba Wax | Repel Shield | Tire Shine | Underbody Wash | T3 Conditioner | Wheel Cleaner'/>
+        <Card title='Exterior#1' description=' Repel Shield | Tire Shine | Underbody Wash | T3 Conditioner | Wheel Cleaner'/>
+        <Card title='Platinium' description='Platinum Seal | Wheel Polish | HotShine® Carnauba Wax | Repel Shield | Tire Shine | Underbody Wash | T3 Conditioner | Wheel Cleaner'/>
       </div>
     </div>
   )
