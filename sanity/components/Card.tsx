8bd323e6f5b498072e816dcfaf7ff6dd50632430
zack <zackzbradshaw@gmail.com>
2023-02-10 19:57:29 -0600

    Added payment links

--- sanity/components/Card.tsx
+++ sanity/components/Card.tsx
@@ -1,15 +1,21 @@
 
 import React from 'react';
-const Card = ({  title, description  }) => {
+import Link from 'next/link'
+const Card = ({  title, description, link  }) => {
+  
   return (
-    <div className="card w-96 h-[300px] bg-base-100 shadow-xl">
+    <div className="card w-96 h-[300px] w-[300px] bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{title}</h2>
         <p>{description}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">
+            <Link href={link}>
+              Buy Now
+            </Link>
+          </button>
         </div>
       </div>
     </div>
--- sanity/components/Services.tsx
+++ sanity/components/Services.tsx
@@ -4,12 +4,11 @@ import Card from './Card'
 function Services() {
   return (
     <div className='flex justify-center items-center bg-black align-center'>
-      <div className=' flex justify-around align-center gap-3 items-center max-w-screen-2xl max-[800px]:flex-col m-5 w-full '>
-
-        <Card title='Basic Exterior' description=' T3 Conditioner | Wheel Cleaner'/>
-        <Card title='Exterior#2' description='HotShine® Carnauba Wax | Repel Shield | Tire Shine | Underbody Wash | T3 Conditioner | Wheel Cleaner'/>
-        <Card title='Exterior#1' description=' Repel Shield | Tire Shine | Underbody Wash | T3 Conditioner | Wheel Cleaner'/>
-        <Card title='Platinium' description='Platinum Seal | Wheel Polish | HotShine® Carnauba Wax | Repel Shield | Tire Shine | Underbody Wash | T3 Conditioner | Wheel Cleaner'/>
+      <div className=' flex  gap-3 justify-center items-center max-w-screen-2xl flex-wrap m-5 w-full '> 
+        <Card title='Basic Exterior' description=' T3 Conditioner | Wheel Cleaner' link='https://buy.stripe.com/test_fZe4iAe4Rasf8sofYY'/>
+        <Card title='Exterior#2' description='HotShine® Carnauba Wax | Repel Shield | Tire Shine | Underbody Wash | T3 Conditioner | Wheel Cleaner'link='https://buy.stripe.com/test_5kAbL2bWJeIv6kg145'/>
+        <Card title='Exterior#1' description=' Repel Shield | Tire Shine | Underbody Wash | T3 Conditioner | Wheel Cleaner' link='https://buy.stripe.com/test_9AQ16o6Cp6bZ384bIK'/>
+        <Card title='Platinium' description='Platinum Seal | Wheel Polish | HotShine® Carnauba Wax | Repel Shield | Tire Shine | Underbody Wash | T3 Conditioner | Wheel Cleaner'link='https://buy.stripe.com/test_fZe2asbWJ9ob9wsfZ1'/>
       </div>
     </div>
   )
