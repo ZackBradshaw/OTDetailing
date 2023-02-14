187220b4e74c3cbcf1802b6c94b30366d4985f63
zack <zackzbradshaw@gmail.com>
2023-02-13 21:20:08 -0600

    Removed unessesarry dropdown

--- sanity/components/Navbar.tsx
+++ sanity/components/Navbar.tsx
@@ -5,25 +5,7 @@ const Navbar = () => {
    <div>
       <div className="navbar bg-base-100  flex justify-center">
         <div className="max-w-screen-2xl w-full flex justify-between">
-        <div className="navbar-start w-fit">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn-ghost btn lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label> 
-          </div>
+        <div className="navbar-start w-fit"> 
           <a className="btn-ghost btn text-xl normal-case">OT Detailing</a>
         </div>
         <div className="navbar-center hidden lg:flex">
