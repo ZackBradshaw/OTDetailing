ac77a130f8b17d388889b7373c6e26b5c0d9ef15
zack <zack@zackbradshaw.com>
2023-03-13 20:53:08 -0500

    removed shoping cart

--- sanity/components/Navbar.tsx
+++ sanity/components/Navbar.tsx
@@ -25,15 +25,15 @@ const Navbar = () => {
               <span className="badge badge-sm indicator-item">8</span>
             </div>
           </label>
-          <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
-            <div className="card-body">
-              <span className="font-bold text-lg">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div>
+          {/*<div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">*/}
+          {/*  <div className="card-body">*/}
+          {/*    <span className="font-bold text-lg">8 Items</span>*/}
+          {/*    <span className="text-info">Subtotal: $999</span>*/}
+          {/*    <div className="card-actions">*/}
+          {/*      <button className="btn btn-primary btn-block">View cart</button>*/}
+          {/*    </div>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
         </div>
         <div> 
           <div className="dropdown dropdown-end"> 
