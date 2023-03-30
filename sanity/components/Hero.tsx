f28c63805b1b05c71f8e37fdf8c3d8d4e6fd7754
zack <zack@zackbradshaw.com>
2023-03-30 18:27:35 -0500

    New mission statement

--- sanity/components/Hero.tsx
+++ sanity/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
       <h1 className="mb-5 text-5xl font-bold">Our Mission</h1>
-      <p className="mb-5">is to provide the best car detailing services with exceptional customer service. Using the latest tools and techniques, we aim to bring out the best in every vehicle. We strive for convenience and professionalism, building lasting relationships based on trust and quality results.</p>
+      <p className="mb-5">Transforming your vehicle through eco-conscious, expert detailing services focused on exceeding customer expectations.</p>
     </div>
   </div>
 </div>
