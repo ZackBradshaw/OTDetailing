6f8c61b9c9562b01f0b78323cb2dc9f8c23883e0
Zack <zack@zackbradshaw.com>
2023-04-30 17:49:38 -0500

    update: Align text

--- sanity/components/Hero.tsx
+++ sanity/components/Hero.tsx
@@ -2,9 +2,9 @@ const Hero = ({ image, title, descriptions, }) => {
   return ( 
 <div className="hero min-h-[50vh] text-secondary content-fill" style={{ backgroundImage: `url(${image})` }}>
   <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content align-start max-w-screen-2x1 w-full text-neutral-content">
-      <h1 className="mb-5 text-5xl align-start ml-[10%] max-w-screen-2x1 font-bold">{title}</h1>
-      <p className="mb-5 max-w-screen-2x1 w-full align-start">{descriptions}</p>
+  <div className="hero-content align-left max-w-screen-2x1 w-full text-neutral-content flex flex-col">
+    <h1 className="text-6xl align-start w-full max-w-screen-2x1 font-bold">{title}</h1>
+      <p className="mb-5 text-4xl max-w-screen-2x1 w-full align-start">{descriptions}</p>
   </div>
 </div>
   );
