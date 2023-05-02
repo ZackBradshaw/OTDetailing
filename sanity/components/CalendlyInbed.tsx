9023348f6c3077791cf8c25adc808f38f95c0635
Zack <zack@zackbradshaw.com>
2023-05-01 19:41:20 -0500

    fix: fix deployment issues

--- sanity/components/CalendlyInbed.tsx
+++ sanity/components/CalendlyInbed.tsx
@@ -17,7 +17,7 @@ const CalendlyEmbed = () => {
     <div>
       <iframe width='100%' height='750px'
               src='https://zackbradshaw.zohobookings.com/portal-embed#/customer/4504693000000031018' frameBorder='0'
-              allowFullScreen=false>
+              >
       </iframe>
       <div
         className="calendly-inline-widget  min-w-[320px=] h-[630px]"
