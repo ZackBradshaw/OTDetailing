1a77ab9b281b38085fb55a80041286405c0911c7
Zack <zack@zackbradshaw.com>
2023-05-17 17:01:20 -0500

    feat: Add booking system

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import Layout from '../sanity/components/Layout'
-import Hero from '../sanity/components/Hero'
 import Services from '../sanity/components/Services'
-import CalendlyInbed from '../sanity/components/CalendlyInbed'
 import Carousel from '../sanity/components/Carousel'
-import Card from '../sanity/components/Card'
+import BookingComponent from '../sanity/components/BookingComponent'
 
 export default function IndexPage() {
   return (
