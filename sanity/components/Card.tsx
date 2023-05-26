b256e44ad8d11d18e179be0bc0688eb8434bdad6
Zack <zack@zackbradshaw.com>
2023-05-26 16:42:00 -0500

    feat: Add responsive design

--- sanity/components/Card.tsx
+++ sanity/components/Card.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link'
 const Card = ({  title, link  }) => {
   
   return (
-<div className="card glass lg:w-[20rem] text-center bg-opacity-30 rounded-sm ">
+<div className="card glass lg:w-[20rem] h-[10vh] text-center flex justify-center bg-opacity-30 rounded-sm ">
     <a href={link}>
-        <div className="card-body text-center h-[10vh] pt-[12%] items-center p-1">
-          <h2 className='card-title align-center text-[2rem]' >{title}</h2>
+        <div className="card-body flex text-center items-center p-1">
+          <h2 className='card-title text-[2rem]' >{title}</h2>
       </div>
     </a>
 </div>
