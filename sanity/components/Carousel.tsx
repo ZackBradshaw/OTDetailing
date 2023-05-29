2413784f160c77c76dc0510fd58793e2077df8fe
Zack <zack@zackbradshaw.com>
2023-05-28 20:47:55 -0500

    feat: Add hero images

--- sanity/components/Carousel.tsx
+++ sanity/components/Carousel.tsx
@@ -10,13 +10,10 @@ export default function Carousel() {
       <Hero image={"https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} descriptions={'Superior service at affordable price'} title={'Exterior'} />
     </div>
     <div id="item2" className="carousel-item w-full">
-      <Hero image={"https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} descriptions={'Superior service at affordable price'} title={'Exterior'} />
+      <Hero image={"/Interior.jpg"} descriptions={'Superior service at affordable price'} title={'Interior'} />
     </div>
     <div id="item3" className="carousel-item w-full">
-        <Hero image={"https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} descriptions={'Superior service at affordable price'} title={'Exterior'} />
-      </div>
-      <div id="item4" className="carousel-item w-full">
-        <Hero image={"https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} descriptions={'Superior service at affordable price'} title={'Exterior'} />
+        <Hero image={"/Wax.jpg"} descriptions={'Superior service at affordable price'} title={'Miscellaneous'} />
       </div>
       <div className="flex flex-col justify-center flex-start absolute bottom-[18%] z-10 w-full py-2 gap-3 lg:flex-row ">
         <Card link={'#item1'} title={'Exterior'} />
