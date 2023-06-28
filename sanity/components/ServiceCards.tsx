959fccee86f87f657d8cdd3020cde343a87e5f10
Zack <zack@zackbradshaw.com>
2023-06-28 18:20:27 -0500

    fix: Made service cards uniform

--- sanity/components/ServiceCards.tsx
+++ sanity/components/ServiceCards.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const ServiceCards = ({ image,  service }) => {
   return (
     <div className='text-black w-[80%] h-[30%] card lg:card-side bg-base-100 shadow-xl'>
-      <figure><img className='object-cover w-full h-full' src={image} alt='Album' /></figure>
-      <div className='card-body'>
+      <figure><img className='object-cover w-[600px] h-[600px]' src={image} alt='Album' /></figure>
+      <div className='card-body '>
         <h2 className='card-title'>Services</h2>
         <div>{service}</div>
         <div className='card-actions w-full'>
