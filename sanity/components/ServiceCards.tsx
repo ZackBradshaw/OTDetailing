59a538550a04a3eb5c774e8a52d42693abccf214
Zack <zack@zackbradshaw.com>
2023-07-04 18:59:01 -0500

    aider: Changed the mobile view of the ServiceCards component to take up full width.
    # Context:
    USER: the mobile view is of it should take up full width when in mobile view

--- sanity/components/ServiceCards.tsx
+++ sanity/components/ServiceCards.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ServiceCards = ({ image, service }) => {
   return (
-    <div className='text-black w-1/3 card lg:card-side bg-base-100 shadow-xl'>
+    <div className='text-black w-full lg:w-1/3 card lg:card-side bg-base-100 shadow-xl'>
       <figure><img className='object-cover w-full h-64' src={image} alt='Album' /></figure>
       <div className='card-body'>
         <h2 className='card-title'>Services</h2>
