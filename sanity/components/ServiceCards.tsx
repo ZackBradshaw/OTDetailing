fe10f00b59569b5ad3637b75a488e96910b845ab
Zack <zack@zackbradshaw.com>
2023-07-04 18:57:57 -0500

    aider: Refactor ServiceCards component to make the images larger and adjust the text size to fit within the card.
    # Context:
    USER: these images are way to small and the text runs off the card the images should be a good size relitive to the page dimemtnions and device dimementions refactor the component

--- sanity/components/ServiceCards.tsx
+++ sanity/components/ServiceCards.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ServiceCards = ({ image, service }) => {
   return (
-    <div className='text-black w-96 h-96 card lg:card-side bg-base-100 shadow-xl'>
-      <figure><img className='object-cover w-full h-48' src={image} alt='Album' /></figure>
+    <div className='text-black w-1/3 card lg:card-side bg-base-100 shadow-xl'>
+      <figure><img className='object-cover w-full h-64' src={image} alt='Album' /></figure>
       <div className='card-body'>
         <h2 className='card-title'>Services</h2>
-        <div>{service}</div>
+        <div className='card-text'>{service}</div>
         <div className='card-actions w-full'>
           <button className='btn btn-primary w-full'>Book Now</button>
         </div>
