2fe57550f7a679c9ecb0fe83873afe4c7b014070
Zack <zack@zackbradshaw.com>
2023-07-04 18:54:53 -0500

    aider: Fix ServiceCards component to make all cards uniform in size, by setting a fixed width and height for the card and image.
    # Context:
    USER: This compoment keep streching the images and setting the compoment to diffrent sizes based on the image size fix this component so that all the cards are uniform no matter image size

--- sanity/components/ServiceCards.tsx
+++ sanity/components/ServiceCards.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-
-const ServiceCards = ({ image,  service }) => {
+const ServiceCards = ({ image, service }) => {
   return (
-    <div className='text-black w-[80%] h-[30%] card lg:card-side bg-base-100 shadow-xl'>
-      <figure><img className='object-cover w-[600px] h-[600px]' src={image} alt='Album' /></figure>
-      <div className='card-body '>
+    <div className='text-black w-96 h-96 card lg:card-side bg-base-100 shadow-xl'>
+      <figure><img className='object-cover w-full h-48' src={image} alt='Album' /></figure>
+      <div className='card-body'>
         <h2 className='card-title'>Services</h2>
         <div>{service}</div>
         <div className='card-actions w-full'>
@@ -13,7 +12,6 @@ const ServiceCards = ({ image,  service }) => {
         </div>
       </div>
     </div>
-
   )
 }
 
