fcab6a4f5c090c7aa4bb806832989b99a75d7e6d
Zack <zack@zackbradshaw.com>
2023-07-09 15:04:22 -0500

    feat: Refactored the `BeforeAfterSlider` component to use the `image-comparison` library.

--- sanity/components/BeforeAfterSlider.tsx
+++ sanity/components/BeforeAfterSlider.tsx
@@ -1,34 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import ImageComparison from 'image-comparison';
 
 const BeforeAfterSlider = ({ beforeImage, afterImage }) => {
-  const [sliderPosition, setSliderPosition] = useState(50);
-
-  useEffect(() => {
-    const topImage = document.getElementById('top-image');
-    if (topImage) {
-      topImage.style.width = `${sliderPosition}%`;
-    }
-  }, [sliderPosition]);
-
-  return (
-    <div className="relative">
-      <img src={beforeImage} alt="Before" className="w-full" />
-      <img
-        id="top-image"
-        src={afterImage}
-        alt="After"
-        className="absolute top-0"
-      />
-      <input
-        type="range"
-        min="0"
-        max="100"
-        value={sliderPosition}
-        onChange={(e) => setSliderPosition(e.target.value)}
-        className="absolute bottom-0 w-full"
-      />
-    </div>
-  );
+    return (
+        <ImageComparison 
+            leftImage={beforeImage} 
+            rightImage={afterImage} 
+            sliderLineWidth={2} 
+            sliderButtonWidth={30} 
+            sliderButtonHeight={30} 
+            handleSliderDrag={null} 
+            handleSliderHover={null} 
+            handleSliderClick={null} 
+        />
+    );
 };
 
 export default BeforeAfterSlider;
