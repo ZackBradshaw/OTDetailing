d4bd4f3b0e11b555108c606cd756e56cfef961e2
Zack <zack@zackbradshaw.com>
2023-07-12 18:44:48 -0500

    Refactor BeforeAfterSlider component to import ImageComparison with correct formatting and spacing.y

--- sanity/components/BeforeAfterSlider.tsx
+++ sanity/components/BeforeAfterSlider.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import ImageComparison from 'image-comparison';
+import  ImageComparison  from "image-comparison";
 
-const BeforeAfterSlider = ({ beforeImage, afterImage }) => {
+const BeforeAfterSlider = ({  beforeImage, afterImage }) => {
     return (
-        <ImageComparison 
-            leftImage={beforeImage} 
-            rightImage={afterImage} 
-            sliderLineWidth={2} 
-            sliderButtonWidth={30} 
-            sliderButtonHeight={30} 
-            handleSliderDrag={null} 
-            handleSliderHover={null} 
-            handleSliderClick={null} 
+        <ImageComparison
+            leftImage={beforeImage}
+            rightImage={afterImage}
+            sliderLineWidth={2}
+            sliderButtonWidth={30}
+            sliderButtonHeight={30}
+            handleSliderDrag={null}
+            handleSliderHover={null}
+            handleSliderClick={null}
         />
     );
 };
