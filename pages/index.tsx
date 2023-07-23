b4b6b17b353a463234dbe9bf47309fa9ed2b0de3
Zack <zack@zackbradshaw.com>
2023-07-22 20:43:33 -0500

    feat: Cut on wanted feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,11 +19,11 @@ export default function IndexPage() {
         </div>
         in the decription field fill in interior services
         <img src="Steps.png" />
-        <ServiceCards
-          image={'https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'}
-          service={<DetailingServices serviceType={'exterior'} />} />
-        <ServiceCards image={'Interior.jpg'} service={<DetailingServices serviceType={'interior'} />} />
-        <ServiceCards image={'Wax.jpg'} service={<DetailingServices serviceType={'miscellaneous'} />} />
+        {/*<ServiceCards*/}
+        {/*  image={'https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'}*/}
+        {/*  service={<DetailingServices serviceType={'exterior'} />} />*/}
+        {/*<ServiceCards image={'Interior.jpg'} service={<DetailingServices serviceType={'interior'} />} />*/}
+        {/*<ServiceCards image={'Wax.jpg'} service={<DetailingServices serviceType={'miscellaneous'} />} />*/}
         <ReactCompareSlider
         itemOne={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image one" />}
         itemTwo={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image two" />}
--- sanity/components/Hero.tsx
+++ sanity/components/Hero.tsx
@@ -1,13 +1,13 @@
-const Hero = ({ image, title, descriptions, }) => {
-  return ( 
-<div className="hero min-h-[50vh] text-secondary content-fill" style={{ backgroundImage: `url(${image})` }}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content align-left pb-[50vh] max-w-screen-2x1 w-full text-neutral-content flex flex-col">
-    <h1 className="text-6xl align-start w-full max-w-screen-2x1 font-bold">{title}</h1>
-      <p className="mb-5 text-4xl max-w-screen-2x1 w-full align-start">{descriptions}</p>
-  </div>
-</div>
-  );
-};
+const Hero = ({ image, title, descriptions }) => {
+  return (
+    <div className='hero min-h-[50vh] text-secondary content-fill' style={{ backgroundImage: `url(${image})` }}>
+      <div className='hero-overlay bg-opacity-60'></div>
+      <div className='hero-content align-left pb-[50vh] max-w-screen-2x1 w-full text-neutral-content flex flex-col'>
+        <h1 className='text-6xl align-start w-full max-w-screen-2x1 font-bold'>{title}</h1>
+        <p className='mb-5 text-4xl max-w-screen-2x1 w-full align-start'>{descriptions}</p>
+      </div>
+    </div>
+  )
+}
 
-export default Hero;
+export default Hero
