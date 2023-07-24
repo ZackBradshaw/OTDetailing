ed15defb7fd43e854c2097f00a74f824f5986b6d
Zack <zack@zackbradshaw.com>
2023-07-23 19:26:02 -0500

    fix: Remove necessary file

--- sanity/components/Carousel.tsx
+++ sanity/components/Carousel.tsx
@@ -17,9 +17,9 @@ export default function Carousel({ items }) {
       {items.map((item, index) => (
         <div id={`slide${index+1}`} className={`carousel-item relative w-full ${currentSlide === index+1 ? 'active' : ''}`}>
           {item}
-          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 top-1/2 space-x-2">
+          <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 bottom-0 space-x-2">
             {items.map((_, i) => (
-              <button onClick={() => setCurrentSlide(i+1)} className={`w-3 h-3 rounded-full ${currentSlide === i+1 ? 'bg-blue-500' : 'bg-gray-300'}`}></button>
+              <button onClick={() => setCurrentSlide(i+1)} className={`w-3 h-3 rounded-full ${currentSlide === i+1 ? 'bg-green-500' : 'bg-gray-300'}`}></button>
             ))}
           </div>
         </div>
