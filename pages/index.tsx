9ccdd9fadfd5e01d2e0d7c47523a85a18eba62b4
Zack <zack@zackbradshaw.com>
2023-11-09 15:24:31 -0600

    feat: remove unusede components

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,16 +51,16 @@ export default function IndexPage() {
           <Services />
           <BookingComponent />
         </div>
-        <ServiceCards image={'https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'} service={<DetailingServices serviceType={'exterior'}/>}/>
-        <ServiceCards image={'Interior.jpg'} service={<DetailingServices serviceType={'interior'}/>}/>
-        <ServiceCards image={'Wax.jpg'} service={<DetailingServices serviceType={'miscellaneous'}/>}/>
+        {/* <ServiceCards image={'https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'} service={<DetailingServices serviceType={'exterior'}/>}/> */}
+        {/* <ServiceCards image={'Interior.jpg'} service={<DetailingServices serviceType={'interior'}/>}/> */}
+        {/* <ServiceCards image={'Wax.jpg'} service={<DetailingServices serviceType={'miscellaneous'}/>}/> */}
         <Widget
           handleNewUserMessage={handleNewUserMessage}
           title="Chat with our AI"
           subtitle=""
           {...({} as any)}
         />
-        <img src="Steps.png" />
+        {/* <img src="Steps.png" /> */}
       </Layout>
     </>
   )
