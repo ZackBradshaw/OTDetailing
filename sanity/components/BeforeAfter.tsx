aaff16c8832db849d7bf185fdbecc88a47c8b673
zack <zack@zackbradshaw.com>
2024-02-12 15:47:40 -0600

    chore: switch carsoul type

--- sanity/components/BeforeAfter.tsx
+++ sanity/components/BeforeAfter.tsx
@@ -1,48 +1,48 @@
 import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 
 const BeforeAfter = () => (
-  <div className="carousel w-full">
-    <div id="slide1" className="carousel-item relative w-full">
-      <ReactCompareSlider
-        itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" alt="Image one" />}
-        itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" alt="Image two" />}
-      />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide4" className="btn btn-circle">❮</a> 
-        <a href="#slide2" className="btn btn-circle">❯</a>
+  <>
+    <div className="carousel max-w-screen-2xl">
+      <div id="slide1" className="carousel-item relative">
+        <div className="compare-slider-container">
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image one" />}
+            itemTwo={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image two" />}
+          />
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image one" />}
+            itemTwo={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image two" />}
+          />
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image one" />}
+            itemTwo={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image two" />}
+          />
+        </div>
       </div>
-    </div> 
-    <div id="slide2" className="carousel-item relative w-full">
-      <ReactCompareSlider
-        itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" alt="Image one" />}
-        itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" alt="Image two" />}
-      />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide1" className="btn btn-circle">❮</a> 
-        <a href="#slide3" className="btn btn-circle">❯</a>
-      </div>
-    </div> 
-    <div id="slide3" className="carousel-item relative w-full">
-      <ReactCompareSlider
-        itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" alt="Image one" />}
-        itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" alt="Image two" />}
-      />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide2" className="btn btn-circle">❮</a> 
-        <a href="#slide4" className="btn btn-circle">❯</a>
-      </div>
-    </div> 
-    <div id="slide4" className="carousel-item relative w-full">
-      <ReactCompareSlider
-        itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" alt="Image one" />}
-        itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" alt="Image two" />}
-      />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide3" className="btn btn-circle">❮</a> 
-        <a href="#slide1" className="btn btn-circle">❯</a>
+      <div id="slide2" className="carousel-item relative">
+        <div className="compare-slider-container">
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" alt="Image one" />}
+            itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" alt="Image two" />}
+          />
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" alt="Image one" />}
+            itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" alt="Image two" />}
+          />
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" alt="Image one" />}
+            itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" alt="Image two" />}
+          />
+        </div>
       </div>
     </div>
-  </div>
+    <div className="flex justify-center w-full py-2 gap-2">
+      <a href="#item1" className="btn btn-xs">1</a>
+      <a href="#item2" className="btn btn-xs">2</a>
+      {/* <a href="#item3" className="btn btn-xs">3</a>
+      <a href="#item4" className="btn btn-xs">4</a> */}
+    </div>
+  </>
 );
 
 export default BeforeAfter
