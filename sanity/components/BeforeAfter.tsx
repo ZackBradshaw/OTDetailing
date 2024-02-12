255c51d0f56b4c9835284e481d67184f32de008c
zack <zack@zackbradshaw.com>
2024-02-12 16:00:35 -0600

    feat: fix before-after styling

--- sanity/components/BeforeAfter.tsx
+++ sanity/components/BeforeAfter.tsx
@@ -2,39 +2,39 @@ import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slide
 
 const BeforeAfter = () => (
   <>
-    <div className="carousel max-w-screen-2xl">
+    <div className="carousel w-full flex flex-row justify-center items-center">
       <div id="slide1" className="carousel-item relative">
-        <div className="compare-slider-container">
+        <div className="compare-slider-container gap-10 flex flex-row justify-center items-center !important">
           <ReactCompareSlider
-            itemOne={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image two" />}
+            itemOne={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image one" style={{ objectFit: 'contain' }} />}
+            itemTwo={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image two" style={{ objectFit: 'contain' }} />}
           />
           <ReactCompareSlider
-            itemOne={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image two" />}
+            itemOne={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image one" style={{ objectFit: 'contain' }} />}
+            itemTwo={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image two" style={{ objectFit: 'contain' }} />}
           />
           <ReactCompareSlider
-            itemOne={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image two" />}
-          />
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative">
-        <div className="compare-slider-container">
-          <ReactCompareSlider
-            itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" alt="Image two" />}
-          />
-          <ReactCompareSlider
-            itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" alt="Image two" />}
-          />
-          <ReactCompareSlider
-            itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" alt="Image two" />}
+            itemOne={<ReactCompareSliderImage src="trunk-after.jpg" alt="Image one" style={{ objectFit: 'contain' }} />}
+            itemTwo={<ReactCompareSliderImage src="trunk-before.jpg" alt="Image two" style={{ objectFit: 'contain' }} />}
           />
         </div>
       </div>
+      {/* <div id="slide2" className="carousel-item relative">
+        <div className="compare-slider-container flex flex-row justify-center items-center !important">
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" alt="Image one" style={{ objectFit: 'contain' }} />}
+            itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" alt="Image two" style={{ objectFit: 'contain' }} />}
+          />
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" alt="Image one" style={{ objectFit: 'contain' }} />}
+            itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" alt="Image two" style={{ objectFit: 'contain' }} />}
+          />
+          <ReactCompareSlider
+            itemOne={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" alt="Image one" style={{ objectFit: 'contain' }} />}
+            itemTwo={<ReactCompareSliderImage src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" alt="Image two" style={{ objectFit: 'contain' }} />}
+          />
+        </div>
+      </div> */}
     </div>
     <div className="flex justify-center w-full py-2 gap-2">
       <a href="#item1" className="btn btn-xs">1</a>
--- sanity/components/Carousel.tsx
+++ sanity/components/Carousel.tsx
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
 import Hero from './Hero'
 
 export default function Carousel() {
+  const [activeItem, setActiveItem] = useState(0);
+  const carouselItems = [
+    {
+      id: "exterior",
+      image: "https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      descriptions: '',
+      title: ''
+    },
+    {
+      id: "interior",
+      image: "/Interior.jpg",
+      descriptions: '',
+      title: ''
+    },
+    {
+      id: "wax",
+      image: "/Wax.jpg",
+      descriptions: '',
+      title: ''
+    }
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveItem((prevActiveItem) => (prevActiveItem + 1) % carouselItems.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-<div className="flex flex-col items-center w-full">
-  <div className="carousel w-full h-[80vh]">
-    <div id="item1" className="carousel-item w-full">
-      <Hero image={"https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} descriptions={''} title={''} />
-    </div>
-    <div id="item2" className="carousel-item w-full">
-      <Hero image={"/Interior.jpg"} descriptions={''} title={''} />
-    </div>
-    <div id="item3" className="carousel-item w-full">
-        <Hero image={"/Wax.jpg"} descriptions={''} title={''} />
-      </div>
-      <div className="flex flex-col justify-center flex-start absolute bottom-[18%] z-10 w-full py-2 gap-3 lg:flex-row ">
-        {/* <Card link={'#item1'} title={'Exterior'} />
-        <Card link={'#item2'} title={'Interior'} />
-        <Card link={'#item3'} title={'Misc'} /> */}
+    <div className="flex flex-col items-center w-full">
+      <div className="carousel w-full h-[80vh]">
+        {carouselItems.map((item, index) => (
+          <div key={item.id} className={`carousel-item w-full ${index === activeItem ? 'block' : 'hidden'}`}>
+            <Hero image={item.image} descriptions={item.descriptions} title={item.title} />
+          </div>
+        ))}
+        <div className="flex flex-col justify-center flex-start absolute bottom-[18%] z-10 w-full py-2 gap-3 lg:flex-row ">
+          {/* Dynamically generate navigation cards if needed */}
+        </div>
       </div>
     </div>
-</div>
-); 
+  ); 
 }
+
