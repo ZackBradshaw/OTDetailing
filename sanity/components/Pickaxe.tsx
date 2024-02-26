d08de7bf4ce22a03cbc25be72099da9e4bda8bef
zack <zack@zackbradshaw.com>
2024-02-26 15:38:09 -0600

    feat: add picaxe

---
+++ sanity/components/Pickaxe.tsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+
+    const Pickaxe = () => {
+        return (
+        <>
+            <link rel="stylesheet" href="https://embed.pickaxeproject.com/axe/scripts/fab/styles.css" />
+            <script src="https://embed.pickaxeproject.com/axe/scripts/fab/bundle.js"></script>
+            <div id="rendering-information" data-height="500px" data-width="500px" data-formid="AutoExpert_Your_Digital_Car_Concierge_RUB22" data-frame-source="https://embed.pickaxeproject.com/axe?id=AutoExpert_Your_Digital_Car_Concierge_RUB22&mode=embed_gold&host=beta&theme=light&opacity=100&font_header=Real+Head+Pro&size_header=30&font_body=Real+Head+Pro&size_body=16&font_labels=Real+Head+Pro&size_labels=14&font_button=Real+Head+Pro&size_button=16&c_fb=FFFFFF&c_ff=FFFFFF&c_fbd=888888&c_bb=228DD7&c_bt=FFFFFF&c_t=000000&s_ffo=100&s_bbo=100&s_f=minimalist&s_b=filled&s_t=2&s_to=1&s_r=2" data-title="Auto-Expert: Your Digital Car Concierge">
+            </div>
+        </>
+        );
+    };
+
+    export default Pickaxe;
