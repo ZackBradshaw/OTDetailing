e4d73c91a5d7fead88185b0b23c046a47c245c0a
zack <zackzbradshaw@gmail.com>
2024-08-12 10:55:00 -0500

    fix: typo

--- sanity/components/GoogleMaps.tsx
+++ sanity/components/GoogleMaps.tsx
@@ -1,26 +1,25 @@
+import React from 'react';
 
-        import React from 'react';
+const GoogleMaps = () => {
+  return (
+    <div className="flex items-center">
+      <iframe
+        className="w-72 h-40"
+        frameBorder="0"
+        style={{ border: 0 }}
+        referrerPolicy="no-referrer-when-downgrade"
+        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBE0K21JMmbww6mBP3fpmVRXeLX7G0AZAA&q=6334+West+Crystal+Lane,+Springfield,+MO&zoom=8&region=us`}
+        allowFullScreen
+      >
+      </iframe>
+      <div className="ml-5 text-b">
+        <p>OT Detailing LLC.</p>
+        <p>Providing reliable service since 2023</p>
+        <p>6334 West Crystal Lane, Springfield, MO</p>
+        <p>Phone: 417-942-9898</p>
+      </div>
+    </div>
+  );
+};
 
-        const GoogleMap = () => {
-          return (
-            <div className="flex items-center">
-              <iframe
-                className="w-72 h-40"
-                frameBorder="0"
-                style={{ border: 0 }}
-                referrerPolicy="no-referrer-when-downgrade"
-                src={`https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_API_KEY}&q=6334+West+Crystal+Lane,+Springfield,+MO&zoom=8&region=us`}
-                allowFullScreen
-              >
-              </iframe>
-              <div className="ml-5 text-b">
-                <p>OT Detailing LLC.</p>
-                <p>Providing reliable service since 2023</p>
-                <p>6334 West Crystal Lane, Springfield, MO</p>
-                <p>Phone: 417-942-9898</p>
-              </div>
-            </div>
-          );
-        };
-
-        export default GoogleMap;
+export default GoogleMaps;
